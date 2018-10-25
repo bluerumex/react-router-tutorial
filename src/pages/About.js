8957fa930c1da6853e38c7f5e359262632f5e861
bluerumex <bluerumex@naver.com>
2018-10-25 12:29:03 +0900

    query-string lib install

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import queryString from 'query-string';
 
-const About = () => {
+const About = ({location, match}) => {
+    const query = queryString.parse(location.search);
+    console.log(query);
+
     return (
         <div>
-            <h2>About</h2>
-            <p>Hello React Router</p>
+            <h2>소개</h2>
+            <p>{match.params.name}</p>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>HOME</h2>
+            <h2>홈</h2>
         </div>
     )
 }
